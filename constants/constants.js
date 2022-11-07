989b34fa513e63fea5d16373c4d9acef507d5c04
javilian98 <javierlian98@gmail.com>
2022-11-07 20:31:17 +0800

    added port 8080 to SERVER_URL

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const CLIENT_URL = 'https://food-recipe-app-chi.vercel.app'
-export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com'
+export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com:8080'
 
 export const API_URL = 'https://api.spoonacular.com'
 export const RECIPES = 'recipes'
