fb384afe21816d1c93231084404c40a74bbd2b02
javilian98 <javierlian98@gmail.com>
2022-11-07 18:42:56 +0800

    replace client localhost url to vercel url

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-export const CLIENT_URL = 'http://localhost:3000'
+export const CLIENT_URL = 'https://food-recipe-app-chi.vercel.app/'
 export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com'
 
 export const API_URL = 'https://api.spoonacular.com'
