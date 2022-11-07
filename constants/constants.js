af2215cce3c5ac1bfc21f07a4972e3aa40a9a09b
javilian98 <javierlian98@gmail.com>
2022-11-07 18:38:42 +0800

    test vercel deployment

--- constants/constants.js
+++ constants/constants.js
@@ -3,4 +3,4 @@ export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.co
 
 export const API_URL = 'https://api.spoonacular.com'
 export const RECIPES = 'recipes'
-export const RECIPES_RANDOM = 'random'
+export const RECIPES_RANDOM = 'random' 
