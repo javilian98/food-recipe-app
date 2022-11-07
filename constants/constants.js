c740f2a2f265eff417a715d829d9117472517a82
javilian98 <javierlian98@gmail.com>
2022-11-07 23:07:22 +0800

    added https to server_url

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const CLIENT_URL = 'https://food-recipe-app-chi.vercel.app'
-export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com:8081'
+export const SERVER_URL = 'https://ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com:8081'
 
 // export const CLIENT_URL = 'http://localhost:3000'
 // export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com:8080'
