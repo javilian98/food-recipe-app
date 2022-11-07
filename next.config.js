775f85380bec664fb8ee2d60cbd73b83740e89e0
javilian98 <javierlian98@gmail.com>
2022-11-07 21:10:34 +0800

    updated next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+import { CLIENT_URL } from './constants/constants'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${CLIENT_URL}/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
