9fe946cc92eba10a43b363488a23ca29f692f388
javilian98 <javierlian98@gmail.com>
2022-11-07 18:27:48 +0800

    tensorflow dependencies into package.json

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const CLIENT_URL = 'http://localhost:3000'
-export const SERVER_URL = 'http://localhost:8080'
+export const SERVER_URL = 'ec2-13-229-125-36.ap-southeast-1.compute.amazonaws.com'
 
 export const API_URL = 'https://api.spoonacular.com'
 export const RECIPES = 'recipes'
